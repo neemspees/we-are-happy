61a2705aeaf800e006df948f1cf74174e302ead9
kim-keypress <kim@keypress.be>
2020-12-01 21:26:39 +0100

    :sparkles: register vote repository to ioc container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Repositories\Interfaces\IVoteRepository;
+use App\Repositories\VoteRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $bindings = [
+        IVoteRepository::class => VoteRepository::class
+    ];
+
     /**
      * Register any application services.
      *
