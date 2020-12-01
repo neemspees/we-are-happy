14dcca87f731c75772dbe75c37f7da1b4976f408
kim-keypress <kim@keypress.be>
2020-12-01 02:31:33 +0100

    :sparkles: add API authentication

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,11 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = [
-            ['name' => 'Employee 1', 'email' => 'employee1@happy.test', 'password' => 'pass'],
-            ['name' => 'Employee 2', 'email' => 'employee2@happy.test', 'password' => 'pass'],
-            ['name' => 'Employee 3', 'email' => 'employee3@happy.test', 'password' => 'pass'],
-            ['name' => 'Manager 1', 'email' => 'manager1@happy.test', 'password' => 'pass'],
-            ['name' => 'Manager 2', 'email' => 'manager2@happy.test', 'password' => 'pass']
+            ['name' => 'Employee 1', 'email' => 'employee1@happy.test', 'password' => 'pass', 'api_token' => 'EMPLOYEE1_TEST_TOKEN'],
+            ['name' => 'Employee 2', 'email' => 'employee2@happy.test', 'password' => 'pass', 'api_token' => 'EMPLOYEE2_TEST_TOKEN'],
+            ['name' => 'Employee 3', 'email' => 'employee3@happy.test', 'password' => 'pass', 'api_token' => 'EMPLOYEE3_TEST_TOKEN'],
+            ['name' => 'Manager 1', 'email' => 'manager1@happy.test', 'password' => 'pass', 'api_token' => 'MANAGER1_TEST_TOKEN'],
+            ['name' => 'Manager 2', 'email' => 'manager2@happy.test', 'password' => 'pass', 'api_token' => 'MANAGER1_TEST_TOKEN']
         ];
 
         foreach ($users as $user) {
@@ -33,7 +33,8 @@ class UserSeeder extends Seeder
             User::query()->create([
                 'name' => $user['name'],
                 'email' => $user['email'],
-                'password' => Hash::make($user['password'])
+                'password' => Hash::make($user['password']),
+                'api_token' => hash('sha256', $user['api_token'])
             ]);
         }
     }
